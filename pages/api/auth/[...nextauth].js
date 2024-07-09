960bdff9a8c900edd62c434149bebe882deaba8c
Surya9122prakash <sspsurya2002@gmail.com>
2024-07-09 08:06:33 +0530

    first commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,31 +1,15 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import clientPromise from '@/lib/mongodb'
+import { MongoDBAdapter } from '@auth/mongodb-adapter'
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientSecret: process.env.GOOGLE_SECRET
     })
   ],
-  // Optional debug mode
-  debug: true,
-  callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      console.log('Sign In:', { user, account, profile, email, credentials });
-      return true;
-    },
-    async redirect({ url, baseUrl }) {
-      console.log('Redirect:', { url, baseUrl });
-      return baseUrl;
-    },
-    async session({ session, token, user }) {
-      console.log('Session:', { session, token, user });
-      return session;
-    },
-    async jwt({ token, user, account, profile, isNewUser }) {
-      console.log('JWT:', { token, user, account, profile, isNewUser });
-      return token;
-    }
-  }
-});
+  adapter: MongoDBAdapter(clientPromise),
+  secret: process.env.NEXTAUTH_SECRET,
+})
